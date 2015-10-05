dd38acee12aa3b7d9cbf80d2316c50e9d3519f9e
vaibhav_etech <vaibhav@excellencetechnologies.in>
2015-10-05 11:32:49 +0530

    graph updated

--- app/graph/graphCtrl.js
+++ app/graph/graphCtrl.js
@@ -8,79 +8,41 @@ graphMod.controller('graphCtrl', function (chord, ajaxRequest, $scope, $rootScop
     console.log($stateParams.graph);
     $scope.view = $stateParams.graph + ' Dependency Graph'; //setting the page heading
     window.document.title = $scope.view;
+
+    $scope.spinner = true;   // hiding and showing spinner ,for charts loading
+    var Chord = chord;       //Chord service variable
+    //function to create graph 
+    self.graph = function (dataset) {
+        var chartEl = document.getElementById('demo');
+
+        // Creates chord diagram and draws to element
+        var chord = new Chord(chartEl);
+        chord.draw(dataset);
+    };
+    //checking url according to the view
     if ($stateParams.graph == 'Capabilities')
     {
 
-        example2(); //funcion for capabilities graph
+        url = "depexplorer/capabilities";
     }
     else
     {
 
-        example1(); //funcion for Objecive graph
+        url = "depexplorer/objectives";
 
     }
-    $scope.spinner = true;   // hiding and showing spinner ,for charts loading
-    var Chord = chord;
-    // first basic example
-    function example1() {
-        console.log(1);
-        var url = "depexplorer/capabilities";
+
+    //funcion to get graph data and call graph() to create graph
+    self.ajax = function (url) {
         var promise = ajaxRequest.send(url);
         promise.then(function (data) {
             $scope.spinner = false;
-            var chartEl = document.getElementById('demo').appendChild(document.createElement('div'));
-
-            chartEl.className = 'chart';
-
-            var chord = new Chord(chartEl);
-            chord.draw(data);
+            self.graph(data);
         });
         promise.catch(function (e) {
             console.log(e);
         });
-
-    }
-
-
-    // second example
-    function example2() {
-        console.log(2);
-        var url = "depexplorer/objectives";
-        var promise = ajaxRequest.send(url);
-        promise.then(function (data) {
-            $scope.spinner = false;
-            var chartEl = document.getElementById('demo').appendChild(document.createElement('div'));
-            chartEl.className = 'chart';
-
-            var options = {
-                outerRadius: 100,
-                innerRadius: 80,
-                padding: 100,
-                labelText: function (d, detail) {
-                    return detail.edata.name;
-                },
-                groupFillColor: function (d, detail) {
-                    return detail.edata.color;
-                },
-                chordFillColor: function (d, sourceDetail, targetDetail) {
-                    return sourceDetail.edata.color;
-                },
-                groupStrokeColor: function () {
-                    return '#999';
-                },
-                chordStrokeColor: function () {
-                    return '#999';
-                }
-            };
-
-            var chord = new Chord(chartEl, options);
-            chord.draw(data);
-
-        });
-        promise.catch(function (e) {
-            console.log(e);
-        });
-    }
-
+    };
+    self.ajax(url);
 
 });
--- app/js/charts/Chord.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileOverview Chart widget that draws chord based on provided dataset
- * @author Demid Nikitin
- * @version: 0.1
- * @module charts/Chord
- */
-define(['d3'], function () {
-
-});
